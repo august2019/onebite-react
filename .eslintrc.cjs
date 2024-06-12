dd0324018e8501143c501d2f7be2b8b0006fe4bb
august2019 <51160844+august2019@users.noreply.github.com>
2024-06-13 00:55:52 +0900

    리엑트 감정일기 처음 커밋
    newpage까지

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "no-unused-vars": "off", //코드상에 실제로 쓰이지않은 변수 오류로 표시
+    "react/prop-types": "off", //리엑트 사용중에 안전하게 사용할수 있게하는
+  },
+}
